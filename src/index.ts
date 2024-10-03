75a86a916c93ddfec249a3a5b4389db845dc6875
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-10-03 08:30:46 -0600

    mover assets a public

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,42 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
-const config = {
+class Example extends Phaser.Scene {
+  objects: undefined;
+  // biome-ignore lint/complexity/noUselessConstructor: <explanation>
+  constructor() {
+    super();
+  }
+
+  preload() {
+    this.load.image('cloud1', 'assets/scenery/overworld/cloud1.png');
+  }
+
+  create() {
+    this.add.image(0, 0, 'cloud1').setOrigin(0, 0).setScale(0.15);
+    // this.objects.image0 = this.add.image(400, 300, 'cloud1');
+  }
+  update(time: number, delta: number): void {}
+}
+
+const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 256,
   height: 224,
-  backgroundColor: "#fff",
-  parent: "game",
-  scene: {
-    preload,
-    create,
-    update,
-  },
+  scene: Example,
+  backgroundColor: '#049cda',
+  parent: 'game',
 };
 
-new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
-function preload() {
-  console.log("preload");
-}
-function create() {
-  console.log("create");
-}
-function update() {
-  // console.log("update");
-}
+// new Phaser.Game(config);
+
+// function preload() {
+//   this.load.image("cloud1", "assets/scenary/overworld/cloud1.png");
+// }
+// function create() {
+//   this.add.image(0, 0, "cloud1");
+// }
+// function update() {
+//   // console.log("update");
+// }
